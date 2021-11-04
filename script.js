4ead4f9f443be78242ee6d564b82fbe632199ba2
rosemarym2 <rosemarymay91@gmail.com>
2021-11-04 10:52:22 +0000

    changes v1.1

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const div = document.querySelectorAll('div');
+const randomNum = document.getElementById("number");
+const score = document.getElementById("score");
+const button = document.getElementById("button");
+const div = document.getElementById("dieface");
+const div = document.querySelectorAll("div");
 
-document.addEventListener ("click", () => {
-    document.getElementById("dieFace").style.visibility = "hidden";
+document.addEventListener ("click", (event) => {
+    document.getElementById("dieFace").style.visibility = "none";
+})
+
+document.querySelector("button").addEventListener("click", (event)=>{
+    document.getElementById("dieface").style.visibility="none";
 })
 
 
-
-
 // button.addEventListener("click", () =>{
-//     let num = Math.floor(Math.random()*6) +1;
-// })
-// console.log(num);
+//     let randomNum = Math.floor(Math.random()*6) +1;
+//     console.log(randomNum);
+// });
