c367c38e43cd77fc456ee3cb68ea129df6964df2
rosemarym2 <rosemarymay91@gmail.com>
2021-11-04 14:16:25 +0000

    changes v1.2

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const randomNum = document.getElementById("number");
-const score = document.getElementById("score");
-const button = document.getElementById("button");
 const dieFace = document.getElementById("dieface");
 const div = document.querySelectorAll("div");
 
+let button = document.getElementById("button");
+let score = document.getElementById("score");
+let randomNum = document.getElementById("number");
+
+const imgs = [
+    "/Dice Images/dice-1.svg",
+    "/Dice Images/dice-2.svg",
+    "/Dice Images/dice-3.svg",
+    "/Dice Images/dice-4.svg",
+    "/Dice Images/dice-5.svg",
+    "/Dice Images/dice-6.svg",
+];
 // button.onclick = function ()={
 
 // }
@@ -22,5 +31,8 @@ const div = document.querySelectorAll("div");
 
 button.addEventListener("click", () =>{
     let randomNum = Math.floor(Math.random()*6) +1;
+    if (randomNum ===1){
+        document.getElementById("die1").src=`${imgs[0]}`
+    }
     console.log(randomNum);
 });
