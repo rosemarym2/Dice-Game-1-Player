c99e810d1eaa149814e489bf41872e61b630218f
rosemarym2 <rosemarymay91@gmail.com>
2021-11-04 16:03:48 +0000

    changes v1.4

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const div = document.querySelectorAll("div");
 let button = document.getElementById("button");
 let score = document.getElementById("score");
 let randomNum = document.getElementById("number");
+let counter = 0;
 
 const imgs = [
     "/Dice Images/dice-1.svg",
@@ -19,8 +20,33 @@ button.addEventListener("click", () =>{
     let randomNum = Math.floor(Math.random()*6) +1;
     if (randomNum ===1){
         document.getElementById("die1").src= "/Dice Images/dice-1.svg";
+        counter = 0;
+        score.innerText = `Total score: ${counter}`;
+    } else if (randomNum ===2){
+        document.getElementById("die2").src= "/Dice Images/dice-2.svg";
+        counter += 2;
+        score.innerText = `Total score: ${counter}`;
+    } else if (randomNum ===3){
+        document.getElementById("die3").src= "/Dice Images/dice-3.svg";
+        counter += 3;
+        score.innerText = `Total score: ${counter}`;
+    } else if (randomNum ===4){
+        document.getElementById("die4").src= "/Dice Images/dice-4.svg";
+        counter += 4;
+        score.innerText = `Total score: ${counter}`;
+    } else if (randomNum ===5){
+        document.getElementById("die5").src= "/Dice Images/dice-5.svg";
+        counter += 5;
+        score.innerText = `Total score: ${counter}`;
+    } else if (randomNum ===6){
+        document.getElementById("die6").src= "/Dice Images/dice-6.svg";
+        counter += 6;
+        score.innerText = `Total score: ${counter}`;
     }
     console.log(randomNum);
+    if (counter >= 21) {
+        score.innerText =`You Won! Total score: ${counter}`;
+    }
 });
 
 // button.onclick = function ()={
