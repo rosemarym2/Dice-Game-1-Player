e931a109b505c44de95f802531ecbe512c6b7fc4
rosemarym2 <rosemarymay91@gmail.com>
2021-11-04 09:32:22 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const div = document.querySelectorAll('div');
+
+document.addEventListener ("click", () => {
+    document.getElementById("dieFace").style.visibility = "hidden";
+})
+
+
+
+
+// button.addEventListener("click", () =>{
+//     let num = Math.floor(Math.random()*6) +1;
+// })
+// console.log(num);
